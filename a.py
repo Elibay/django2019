db1a7bf1ac4f355ac4442eb80d7ace662ac4a645
Elibay <elibay98@gmail.com>
2019-06-04 18:34:32 +0600

    first commit

---
+++ a.py
@@ -0,0 +1,18 @@
+def a_plus_b(a, b):
+  # jhjkhfajksdhfjkhlaf
+  print ("this function a + b")
+  return str(a + b) + " Hello"
+
+  # '' == ""
+def check(a, b):
+  if a > b:
+    return "a gre b"
+  if a < b:
+    return 'a less b'
+  return 'a == b'
+
+def a_minus_b(a, b):
+  print ("hello")
+
+print (a_plus_b(10, 5))
+print (check(10, 10))
